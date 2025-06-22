f7af7a56e4f18c3b36ca76f7b5fde0e3c2ac6105
Zeeshan Ahmad <shanahmad.dev@gmail.com>
2025-06-23 01:14:48 +0500

    added education data

--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -1,4 +1,7 @@
 /* Change this file to get your personal Porfolio */
+import comsats from "../assets/images/comsats.png"
+import cisco from "../assets/images/cisco.png"
+import comsatsATD from "../assets/images/comsatsatd.png"
 
 // Website related settings
 const settings = {
@@ -320,231 +323,50 @@ const skills = {
 const degrees = {
     degrees: [
         {
-            title: "Amrita Vishwa Vidyapeetham, Coimbatore",
-            subtitle: "Masters in Computer Applications (MCA)",
-            logo_path: "amrita_logo.png",
-            alt_name: "Amrita University",
-            duration: "2021 - 2023",
+            title: "Comsats University Islamabad, Abbottabad Campus",
+            subtitle: "BS in Software Engineering",
+            logo: comsats,
+            alt_name: "Comsats University",
+            duration: "2020 - 2024",
             descriptions: [
-                "⚡ I have taken varity of courses related to Artificial Intelligence, Machine Learning, Computer Vision etc.",
+                "⚡ I have taken varity of courses related to Artificial Intelligence, Web Dev, Mobile Dev etc.",
                 "⚡ Apart from this, I have done courses on Cloud Computing and Full Stack Development.",
             ],
-            website_link: "https://www.amrita.edu/",
-        },
-        {
-            title: "Guru Gobind Singh Indraprastha University, New Delhi",
-            subtitle: "Bachelors in Computer Applications (BCA)",
-            logo_path: "ggsipu_logo.png",
-            alt_name: "GGSIPU",
-            duration: "2018 - 2021",
-            descriptions: [
-                "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-                // "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development."
-            ],
-            website_link: "http://www.ipu.ac.in/",
-        },
-    ],
-};
+            website_link: "https://www.cuiatd.edu.pk/",
+        }
+    ]
+
+}
+
 
 const certifications = {
     certifications: [
         {
-            title: "IBM Full Stack Software Developer",
-            subtitle: "",
-            logo_path: "ibm_logo.jpg",
+            title: "Merit Certificate",
+            subtitle: "Recognized for academic excellence as Gold Medalist",
+            logo: comsatsATD,
             certificate_link:
-                "https://coursera.org/share/cbcd30865e98e2e45161a6ace19d3558",
-            alt_name: "IBM",
-            color_code: "#096ce1",
+                "https://drive.google.com/file/d/1NgBX11_jrBWdroBfkYuRlKLfpnUIarvJ/view?usp=sharing",
+            alt_name: "Merit Certificate",
+            color_code: "white",
         },
         {
-            title: "Application Development using Microservices and Serverless",
-            subtitle: "",
-            logo_path: "ibm_logo.jpg",
-            certificate_link:
-                "https://coursera.org/share/4fa1b8d2f654495a0b692b65d9964ad5",
-            alt_name: "IBM",
-            color_code: "#096ce1",
-        },
-        {
-            title: "Introduction to Cloud Computing",
-            subtitle: "",
-            logo_path: "ibm_logo.jpg",
+            title: "CCNAv7",
+            subtitle: "Cisco Certified Network Associate Routing and Switching (CCNA)",
+            logo: cisco,
             certificate_link:
                 "https://coursera.org/share/438a8e16af4786782c8a739aef79e999",
             alt_name: "IBM",
-            color_code: "#096ce1",
+            color_code: "white",
         },
         {
-            title: "Introduction to Containers w/ Docker, Kubernetes & OpenShift",
-            subtitle: "",
-            logo_path: "ibm_logo.jpg",
+            title: "Certificate Of Appreciation",
+            subtitle: "Recognized for leadership and dedicated service as Class Representative",
+            logo: comsatsATD,
             certificate_link:
-                "https://coursera.org/share/cfa7b88b913e4a8446b9dc706c0802b0",
+                "https://drive.google.com/file/d/1sPswsG6D6jtW_YGLc5oSQcO7gj3M7UPT/view?usp=sharing",
             alt_name: "IBM",
-            color_code: "#096ce1",
-        },
-        {
-            title: "Django Application Development with SQL and Databases",
-            subtitle: "",
-            logo_path: "ibm_logo.jpg",
-            certificate_link:
-                "https://coursera.org/share/47d8c39642888094ad458b5e44a3c9f3",
-            alt_name: "IBM",
-            color_code: "#096ce1",
-        },
-        {
-            title: "Developing AI Applications with Python and Flask",
-            subtitle: "",
-            logo_path: "ibm_logo.jpg",
-            certificate_link:
-                "https://coursera.org/share/f4ce59c0d68e4569056faaf2fdb4c256",
-            alt_name: "IBM",
-            color_code: "#096ce1",
-        },
-        {
-            title: "Frontend Developer Capstone",
-            subtitle: "",
-            logo_path: "meta_logo.png",
-            certificate_link:
-                "https://coursera.org/share/f80912b58e3afcfa49e6116210a2c9da",
-            alt_name: "Meta",
-            color_code: "#096ce1",
-        },
-        {
-            title: "Advanced React",
-            subtitle: "",
-            logo_path: "meta_logo.png",
-            certificate_link:
-                "https://coursera.org/share/877acd7eaf9dd532a2cb5b103576967e",
-            alt_name: "Meta",
-            color_code: "#096ce1",
-        },
-        {
-            title: "Java Programming: Build a Recommendation System",
-            subtitle: "",
-            logo_path: "duke-logo.jpg",
-            certificate_link:
-                "https://coursera.org/share/6af001cafc4b683bb38abdb9e81d0d79",
-            alt_name: "Duke University",
-            color_code: "#001a57",
-        },
-        {
-            title: "React Basics",
-            subtitle: "",
-            logo_path: "meta_logo.png",
-            certificate_link:
-                "https://coursera.org/share/29c75a387fcaf2f1e2403f73473092b2",
-            alt_name: "Meta",
-            color_code: "#096ce1",
-        },
-        {
-            title: "Programming with JavaScript",
-            subtitle: "",
-            logo_path: "meta_logo.png",
-            certificate_link:
-                "https://coursera.org/share/cb44f1ee2db3f50b83a56cbae15fcc74",
-            alt_name: "Meta",
-            color_code: "#096ce1",
-        },
-        {
-            title: "HTML and CSS in depth",
-            subtitle: "",
-            logo_path: "meta_logo.png",
-            certificate_link:
-                "https://coursera.org/share/667de02731a480b76b5300c6612d174d",
-            alt_name: "Meta",
-            color_code: "#096ce1",
-        },
-        {
-            title: "Java Programming: Principles of Software Design",
-            subtitle: "",
-            logo_path: "duke-logo.jpg",
-            certificate_link:
-                "https://coursera.org/share/f4e9af47fbb24381e04031f3cc8cfd9f",
-            alt_name: "Duke University",
-            color_code: "#001a57",
-        },
-        {
-            title: "Java Programming: Solving Problems with Software",
-            subtitle: "",
-            logo_path: "duke-logo.jpg",
-            certificate_link:
-                "https://coursera.org/share/36411fcea7ea4fbb5c613ef4911f4668",
-            alt_name: "Duke University",
-            color_code: "#001a57",
-        },
-        {
-            title: "Programming Foundations with JavaScript, HTML and CSS",
-            subtitle: "",
-            logo_path: "duke-logo.jpg",
-            certificate_link:
-                "https://coursera.org/share/0269c76c33997304d1842d4eef16815b",
-            alt_name: "Duke University",
-            color_code: "#001a57",
-        },
-        {
-            title: "Android Application Development - II",
-            subtitle: "- Dr. Adam Porter",
-            logo_path: "maryland_logo.svg",
-            certificate_link:
-                "https://www.coursera.org/account/accomplishments/verify/GN48B7W9TU59",
-            alt_name: "Stanford University",
-            color_code: "#e11a2b",
-        },
-        {
-            title: "Android Application Development - I",
-            subtitle: "- Dr. Adam Porter",
-            logo_path: "maryland_logo.svg",
-            certificate_link:
-                "https://www.coursera.org/account/accomplishments/verify/2H59Z22DDSU8",
-            alt_name: "Stanford University",
-            color_code: "#e11a2b",
-        },
-        {
-            title: "Fullstack Development",
-            subtitle: "- Christian Hur",
-            logo_path: "coursera_logo.png",
-            certificate_link:
-                "https://www.coursera.org/account/accomplishments/verify/97WF8EZ4Y2YD",
-            alt_name: "Coursera",
-            color_code: "#0056d2",
-        },
-        {
-            title: "ML and AI in GCP",
-            subtitle: "- GCP Training",
-            logo_path: "gcp_logo.png",
-            certificate_link:
-                "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2862832",
-            alt_name: "GCP",
-            color_code: "#2fa9508a",
-        },
-        {
-            title: "Foundational Infrastructure in GCP",
-            subtitle: "- GCP Training",
-            logo_path: "gcp_logo.png",
-            certificate_link:
-                "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2653409",
-            alt_name: "GCP",
-            color_code: "#4087f08a",
-        },
-        {
-            title: "Networking & Security in GCP",
-            subtitle: "- GCP Training",
-            logo_path: "gcp_logo.png",
-            certificate_link:
-                "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2510430",
-            alt_name: "GCP",
-            color_code: "#eb40308a",
-        },
-        {
-            title: "Cloud Computing Fundamentals in GCP",
-            subtitle: "- GCP Training",
-            logo_path: "gcp_logo.png",
-            certificate_link:
-                "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2469792",
-            alt_name: "GCP",
-            color_code: "#fabd008a",
+            color_code: "white",
         },
     ],
 };
